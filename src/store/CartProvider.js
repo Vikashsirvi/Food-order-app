ecba3b02836539cdec7c794dfee624785f69fa2c
vikash sirvi <50855117+Vikashsirvi@users.noreply.github.com>
2021-11-27 13:19:06 +0530

    Adding better user feedback

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -56,6 +56,9 @@ const cartReducer = (state, action) => {
       totalAmount: updatedTotalAmount,
     };
   }
+  if (action.type === "CLEAR") {
+    return defaultCartState;
+  }
   return defaultCartState;
 };
 
@@ -76,11 +79,17 @@ const CartProvider = (props) => {
       id: id,
     });
   };
+
+  const clearCartHandler = () => {
+    dispatchCartAction({ type: "CLEAR" });
+  };
+
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
+    clearCart: clearCartHandler,
   };
   return (
     <CartContext.Provider value={cartContext}>
